a92cbc1ff09d54b613c7fbcef208d1a4e5bfe412
Esau Araiza <esau.araiza@gmail.com>
2021-07-28 16:43:10 -0500

    Se agregan modificaciones para cuponera

--- descuentos.js
+++ descuentos.js
@@ -1,3 +1,18 @@
+const coupons = [
+    {
+        name: "nuevo",
+        discount: 30
+    },
+    {
+        name: "frecuente",
+        discount: 10
+    },
+    {
+        name: "recomendado",
+        discount: 15
+    }
+];
+
 function calcularPrecioConDescuento(precio, descuento) {
     const porcentajePrecioConDescuento = 100 - descuento;
     const precioConDescuento =(precio * porcentajePrecioConDescuento)/100;
@@ -17,3 +32,27 @@ function onClickButtonPriceDiscount() {
     const resultP = document.getElementById("ResultP");
     resultP.innerText = `El precio con descuento son: $${precioConDescuento}`;
 }
+
+function getDiscount() {
+    const inputCoupon = document.getElementById("InputCoupon");
+    const couponValue = inputCoupon.value;
+    const inputPrice = document.getElementById("InputPrice");
+    const priceValue = inputPrice.value;
+
+    const isCouponValueValid = function (coupon){
+        return coupon.name === couponValue;
+    };
+
+    const userCoupon = coupons.find(isCouponValueValid);
+
+    if(!userCoupon){
+        alert(`El cupón ${couponValue} no es valido`);
+    }else{
+        const descuento = userCoupon.discount;
+        const precioConDescuento = calcularPrecioConDescuento(priceValue, descuento);
+
+        const resultP = document.getElementById("ResultP");
+        resultP.innerText =`El precio con descuento son: ${precioConDescuento}`;
+    }
+}
+
