4f93a3e80b925a62c60b3082e249960b52653150
Esau Araiza <esau.araiza@gmail.com>
2021-07-28 19:25:32 -0500

    Se agrega la funcion para calcular la moda

---
+++ moda.js
@@ -0,0 +1,34 @@
+// const lista1 = [
+//     1,
+//     2,
+//     3,
+//     1,
+//     2,
+//     3,
+//     4,
+//     2,
+//     2,
+//     2,
+//     1
+// ];
+
+function calcularModa(lista) {
+    const listaCount = {};
+
+    lista.map(function (elemento) {
+        if(listaCount[elemento]){
+            listaCount[elemento]++;
+        }else{
+            listaCount[elemento] = 1;
+        }
+    });
+    
+    const listaArray = Object.entries(listaCount).sort(function (a, b) {
+        return a[1] - b[1];
+    });
+    
+    const moda = listaArray[listaArray.length-1]; 
+    
+    return moda;
+}
+
