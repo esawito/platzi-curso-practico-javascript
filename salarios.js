66f47f524f4d26dcf5188b5a974da79c03f9883b
Esau Araiza <esau.araiza@gmail.com>
2021-08-03 22:14:03 -0500

    Se agrega el analisis de salarios de Mexico

---
+++ salarios.js
@@ -0,0 +1,50 @@
+const mexico = [];
+
+mexico.push({
+    name: "esau",
+    salary: 36000
+});
+
+mexico.push({
+    name: "abda",
+    salary: 16000
+});
+
+mexico.push({
+    name: "asahel",
+    salary: 50000
+});
+
+mexico.push({
+    name: "mauricio",
+    salary: 70000
+});
+
+mexico.push({
+    name: "victor",
+    salary: 100000
+});
+mexico.push({
+    name: "papa",
+    salary: 30000
+});
+mexico.push({
+    name: "mama",
+    salary: 10000
+});
+mexico.push({
+    name: "Raul",
+    salary: 35000
+});
+mexico.push({
+    name: "Arturo",
+    salary: 50000
+});
+mexico.push({
+    name: "Patricia",
+    salary: 30000
+});
+mexico.push({
+    name: "luza",
+    salary: 5000
+});
